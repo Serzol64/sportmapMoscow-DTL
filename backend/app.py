752d252467087d5f9eb701320e7d40b23103ddc7
Сергей Золотарёв <40953791+Serzol64@users.noreply.github.com>
2021-10-23 11:19:02 +0400

    Host Config Update

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import json
 import torch
 import geojson
 from flask import Flask, jsonify
+from waitress import serve
 
 def datasetConnect(sheet):
 	if sheet == 'objects': return pd.read_excel('data/db.xlsx', sheet_name='Объекты отдельно')
@@ -19,13 +20,11 @@ def welcome():
 	#Основной сервис для карты
 
 	objectList = datasetConnect('objects')
-
 	features = objectList.apply( lambda row: Feature(geometry=Point((float(row['Долгота (Longitude)']), float(row['Широта (Latitude)'])))), axis=1).tolist()
-
 	properties = objectList.drop(['Широта (Latitude)', 'Долгота (Longitude)'], axis=1).to_dict()
-
 	feature_collection = FeatureCollection(features=features, properties=properties)
-
 	return jsonify(feature_collection)
 
 
+if __name__ == "__main__":
+    serve(app, host="service.dtl-wcc2021.tk", port=5001)
